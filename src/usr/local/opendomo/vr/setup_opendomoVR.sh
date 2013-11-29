e2cbbc0562964c3baca352bd1d2a91bcf03946bf
Manuel Angel Loureiro <manu@debian.manu.es>
2013-11-29 10:09:00 +0100

    20131129:basic stable

--- src/usr/local/opendomo/vr/setup_opendomoVR.sh
+++ src/usr/local/opendomo/vr/setup_opendomoVR.sh
@@ -8,12 +8,13 @@ CONFIGDIR="/etc/opendomo/speech"
 DIR="$(pwd)"
 #Compiling dictionary.c:
 
-echo Assuming all files are in "'$DIR'"
+#echo Assuming all files are in "'$DIR'"
 
-touch Recognition/dictionary.c
-cd Recognition
-make
-cd ..
+#touch Recognition/dictionary.c
+#cd Recognition
+#make
+#cd ..
+echo "Clean directories from previous installations"
 rm Recognition/modes/main.dic
 rm Recognition/bin/ -r
 rm $CONFIGDIR/plugins.db
@@ -22,8 +23,8 @@ rm $CONFIGDIR/configs -r
 
 #mkdir $HOME/.palaver.d/
 sudo mkdir -p $CONFIGDIR
-sudo chown -R manu:manu $CONFIGDIR
-#Atentos!!...cambiar el owner
+sudo chown -R 1000:1000 $CONFIGDIR
+#Atentos!!...cambiar el owner...Usamos 1000:1000 para ser compatible : opendomo: admin,,, mi instalación: manu
 cp Recognition/config/defaultBin Recognition/bin -r
 #touch $HOME/.palaver.d/UserInfo
 touch $CONFIGDIR/UserInfo
@@ -31,7 +32,8 @@ touch $CONFIGDIR/UserInfo
 cp Recognition/config/BlankInfo $CONFIGDIR/UserInfo
 touch Recognition/modes/main.dic
 cp Recognition/config/defaultMain.dic Recognition/modes/main.dic
-./installDefault
+#Solo instalamos lo específico de Opendomo
+#./installDefault
 ./plugins -i /usr/local/opendomo/voiceCommands/OpenDomo_start/OpenDomo_start.sp
 ./plugins -i /usr/local/opendomo/voiceCommands/OpenDomo_stop/OpenDomo_stop.sp
 ./plugins -i /usr/local/opendomo/voiceCommands/LucesON/LucesON.sp
