05cdf63a925c5bb2112f4cee4a6505d86f16b368
Manuel Angel Loureiro <manu@debian.manu.es>
2014-03-12 19:59:14 +0100

    Consolidate audit and control menu:20140312

--- usr/local/opendomo/vr/audit_speech_detect.sh
+++ usr/local/opendomo/vr/audit_speech_detect.sh
@@ -3,18 +3,25 @@ vrlife=true
 
 while [ $vrlife = true ]
 do
-	#audit every 30 sec
-	sleep 30
+	#audit every 60 sec
+	sleep 60
+	pr=`ps -eaf | grep -c "recognize"`
+	echo "Recognize" $pr	
 	
-	if [ `ps -eaf | grep -c "recording.flac"` -lt 2 ] && [ `ps -eaf | grep -c "autodetect"` -eq 2 ] 
+	if [ `ps -eaf | grep -c "recording.flac"` -lt 2 ] && [ `ps -eaf | grep -c "autodetect"` -ge 2 ] 
 	then
-		p1=`ps -ea |grep autodetect| awk '{ print $1}'`
-		date >> /usr/local/opendomo/vr/AUDIT
-		kill $p1
+		if [ `ps -eaf | grep -c "recognize"` -lt 2 ]
+		then
+			# If there is no process running of recognize (system is not acting, but in voice recognition
+			p1=`ps -ea |grep autodetect| awk '{ print $1}'`
+			date >> /usr/local/opendomo/vr/AUDIT
+			kill $p1
+		fi
 	fi
 	
 	if [ `ps -eaf | grep -c "opendomoVR"` -lt 2 ]
 	then
+		#Suicide after main process is killed
 		vrlife=false
 	fi
 done
